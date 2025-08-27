e447b36697d485383df1dd8fc6e9758e8b74626d
albert <alberix252@gmail.com>
2025-08-27 18:06:27 -0500

    se realizo el js y las operaciones

--- js/main.js
+++ js/main.js
@@ -7,11 +7,57 @@ function simulador(){
     contPost.classList.remove('disabled');
     contPre.classList.add('disabled');
 
+//variables que capturan los valores de los campos
     let cNombre = document.getElementById('nombre').value;
     let cTelefono = document.getElementBy('telefono').value;
     let cEmail = document.getElementById('email').value;
-    
+    let cMonto = document.getElementById('monto').value;
+    let cTiempo = document.getElemetById('tiempo').value;
 
+//constantes que capturan las etiquetas span en las que se muenstran los valores, por eso la s de show sTiempo, sInteres...
+    const sTiempo = document.getElemenbtById('sTiempo');
+    const sInteres = document.getElementById('sInteres');
+    const sTotal = document.getElementById('sTotal');
+    const sGanancia = document.getElementById('sGanancia');
+    const sNombre = document.getElementById('sNombre');
+    const sCorreo = document.getElementById('sCorreo');
+
+    //variables para realizar los calculos
+    let ganancia = 0;
+    let total = 0;
+
+    switch (cTiempo) {
+        
+        //en este case realizaremos el calculo si el tiempo corresponde a un año
+        case '1':
+            ganancia = (cMonto*0.008)*12;
+            total = parseInt(cMonto) + parseInt(ganancia);   
+            sInteres.textContent = '0,8%';
+            sTiempo.textContent = '12 meses';   
+        break;
+        
+        //en este case realizaremos el calculo si el tiempo corresponde a dos años
+        case '2':
+            ganancia = (cMonto*0.013)*24;
+            total = parseInt(cMonto) + parseInt(ganancia);   
+            sInteres.textContent = '1,3%';
+            sTiempo.textContent = '24 meses';  
+        break;
+        
+        //en este case realizaremos el calculo si el tiempo corresponde a tres años
+        case '3':
+            ganancia = (cMonto*0.017)*36;
+            total = parseInt(cMonto) + parseInt(ganancia);   
+            sInteres.textContent = '1,7%';
+            sTiempo.textContent = '36 meses';  
+        break;
+    }
+
+    sGanancia.textContent = ganancia;
+    sTotal.textContent = total;
+    sNombre.textContent = cNombre;
+    sCorreo.textContent = cEmail;
+    
 }
 
 document.addEventListener('DOMContentLoaded',function() {
